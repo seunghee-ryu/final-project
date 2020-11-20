0be257194bf6a18036aa70976fbb0da599148d72
ryu seunghee <ryuseunghee.dev@gmail.com>
2020-11-21 08:41:23 +0900

    docs 201121

--- src/main/java/com/acv/test.java
+++ src/main/java/com/acv/test.java
@@ -1,57 +1,12 @@
 package com.acv;
 
-import java.io.IOException;
-import java.util.Iterator;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-public class test {
-
+public class Test {
+  
   public static void main(String[] args) {
-    // Jsoup를 이용해서 http://www.cgv.co.kr/movies/ 크롤링
-    String url = "https://movie.naver.com/movie/bi/mi/basic.nhn?code=136900"; //크롤링할 url지정
-    Document doc = null;        //Document에는 페이지의 전체 소스가 저장된다
-
-    try {
-      doc = Jsoup.connect(url).get();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    //select를 이용하여 원하는 태그를 선택한다. select는 원하는 값을 가져오기 위한 중요한 기능이다.
-    //    Elements element = doc.select("div.sect-movie-chart");
-    //    Elements element = doc.select("div.mv_info_area");
-    Iterator<Element> iterator = doc.select("dl.step2").iterator();
-    System.out.println(iterator.next().select("a").text());
-    //    영화 제목
-    //    영화 감독
-    //    영화 영문명
-    //    상영시간
-    //    제작연도
-    //    개봉일
-    //    출연
-    //    시놉시스
-    //    국가명
-
-
-    System.out.println("============================================================");
-
-    //Iterator을 사용하여 하나씩 값 가져오기
-    //    Iterator<Element> ie1 = element.select("strong.rank").iterator();
-    //    Iterator<Element> ie2 = element.select("strong.title").iterator();
-    //    System.out.println(element.select("a"));
-    //    Iterator<Element> ie2 = element.select("strong.h_movie2").iterator();
-
-    //    while (ie1.hasNext()) {
-    //      System.out.println(ie1.next().text()+"\t"+ie2.next().text());
-    //    }
-    //    while (ie1.hasNext()) {
-    //      System.out.println(ie1.next().text());
-    //    }
-    //    while (ie2.hasNext()) {
-    //      System.out.println(ie2.next().text());
-    //    }
-    //    System.out.println(ie1.next().text());
-
-    System.out.println("============================================================");
+    String str = "<li class=\"_list\" id=\"6703804\" data-json=\"{&quot;nid&quot;:0, &quot;imageNid&quot;:6703804,&quot;movieCode&quot;:189141,&quot;title&quot;:&quot;&quot;,&quot;imageDesc&quot;:&quot;\\uC601\\uD654 <\\uC0BC\\uC9C4\\uADF8\\uB8F9 \\uC601\\uC5B4\\uD1A0\\uC775\\uBC18> \\uC2A4\\uD2F8 \\uC774\\uBBF8\\uC9C0&quot;,&quot;width&quot;:768,&quot;height&quot;:432,&quot;gdid&quot;:&quot;080005ea_000000664abc&quot;,&quot;adult&quot;:&quot;false&quot;,&quot;fullImageUrl74px&quot;:&quot;https://movie-phinf.pstatic.net/20200929_113/1601360032918GP0Vh_JPEG/movie_image.jpg?type=n74_74_2&quot;,&quot;fullImageUrl665px&quot;:&quot;https://movie-phinf.pstatic.net/20200929_113/1601360032918GP0Vh_JPEG/movie_image.jpg?type=m665_443_2&quot;,&quot;fullImageUrl886px&quot;:&quot;https://movie-phinf.pstatic.net/20200929_113/1601360032918GP0Vh_JPEG/movie_image.jpg?type=m886_590_2&quot;,&quot;viewCount&quot;:22250, &quot;koreanMovieTitle&quot;:&quot;&quot;, &quot;movieYear&quot;:0, &quot;castName&quot;:&quot;&quot;, &quot;koreanPeopleName&quot;:&quot;&quot;,&quot;imageType&quot;:&quot;STILLCUT&quot;,&quot;typeSequence&quot;:17,&quot;addDate&quot;:&quot;1601359935000&quot; }\"><a href=\"#\" title=\"\" class=\"\"><img src=\"https://movie-phinf.pstatic.net/20200929_113/1601360032918GP0Vh_JPEG/movie_image.jpg?type=n74_74_2\" width=\"74\" height=\"74\" alt=\"STILLCUT\"></a><!-- N=a:pvi.img --></li>";
+    
+    System.out.println(str.split("fullImageUrl886px&quot;:&quot;")[1].split("/?")[0]);
+    
   }
+
 }
